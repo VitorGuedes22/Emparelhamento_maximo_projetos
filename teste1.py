64a11cf90a7d3e28b2bb25cc55309a430b3e0c1b
Vitor Guedes <studyprofessional18@gmail.com>
2023-10-15 20:31:44 -0300

    arq teste1.py mod1

--- teste1.py
+++ teste1.py
@@ -1,16 +1,37 @@
-import community
-import networkx as nx
-import matplotlib.pyplot as plt
+def ground_truth(grafo):
+    comunidades = dict()
 
-G = nx.erdos_renyi_graph(30, 0.05)
-partition = community.best_partition(G)
-size = float(len(set(partition.values())))
-pos = nx.spring_layout(G)
-count = 0
-for com in set(partition.values()) :
-    count = count + 1
-    list_nodes = [nodes for nodes in partition.keys()if partition[nodes] == com]
-    nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 20,node_color = str(count / size))
+    for vertice,dictDept in grafo.nodes(data = True):
+        if not dictDept["dept"] in comunidades:
+            comunidades[dictDept["dept"]] = {vertice}
+        else:
+            comunidades[dictDept["dept"]].add(vertice)
 
-nx.draw_networkx_edges(G, pos, alpha=0.5)
-plt.show()
+    return comunidades
+
+def visualizaParticao(grafo,partition,nome):
+    pos = nx.spring_layout(grafo)
+    # color the nodes according to their partition
+    num_partitions = len(partition.values())
+    
+    cmap = cm.get_cmap('viridis', num_partitions + 1)
+
+    cores = {node: cmap(partition[node] / num_partitions) for node in partition}
+    
+    #nx.draw_networkx_nodes(grafo, pos, partition.keys(), node_size=40,cmap=cmap, node_color=list(partition.values()))
+    nx.draw_networkx_nodes(G, pos, node_color=list(cores.values()), node_size=200, cmap=cmap)
+    
+    nx.draw_networkx_edges(grafo, pos, alpha=0.5)
+    plt.show()
+    #plt.savefig(nome + ".png")
+
+def questao7(grafo):
+    #descobrindo a melhor partição com a maior modularidade
+    #particaoLouvain = community_louvain.best_partition(grafo,random_state=123)
+    particaoGroundTruth = ground_truth(grafo)
+
+    #visualizaParticao(grafo,particaoLouvain,"ParticaoLouvain")
+    visualizaParticao(grafo,particaoGroundTruth,"ParticaoGroundTruth")
+    #print(len(groundTruth))
+
+#teste no git
